8f55ae6d700ebc4d0894c4b7afdafa60f40436eb
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2019-12-29 17:51:04 +0100

    coding style: convert spaces to tabs
    Kernel coding style uses tabs, not spaces, for indentation, so fix up
    the .c files to use the correct indentation rules.
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- driver/src/fancontrol.c
+++ driver/src/fancontrol.c
@@ -2,39 +2,37 @@
 
 int get_max_fan_rpm(__u32 product_id)
 {
-    switch (product_id)
-    {
-        case BLADE_2018_ADV:
-        case BLADE_2018_BASE:
-            return MAX_FAN_RPM_2018_BLADE;
-        case BLADE_2017_STEALTH_MID:
-        case BLADE_2017_STEALTH_END:
-        case BLADE_2019_STEALTH:
-            return MAX_FAN_RPM_STEALTH;
-        default:
-            return MAX_FAN_RPM_DEFAULT;
-    }
+	switch (product_id) {
+	case BLADE_2018_ADV:
+	case BLADE_2018_BASE:
+		return MAX_FAN_RPM_2018_BLADE;
+	case BLADE_2017_STEALTH_MID:
+	case BLADE_2017_STEALTH_END:
+	case BLADE_2019_STEALTH:
+		return MAX_FAN_RPM_STEALTH;
+	default:
+		return MAX_FAN_RPM_DEFAULT;
+	}
 }
 
 __u8 clampFanRPM(unsigned int rpm, __u32 product_id)
 {
-    int max_allowed = get_max_fan_rpm(product_id);
-    if (rpm > max_allowed) {
-        return (__u8) (max_allowed / 100);
-    } else if (rpm < ABSOLUTE_MIN_FAN_RPM) {
-        return (__u8) (ABSOLUTE_MIN_FAN_RPM / 100);
-    }
-    return (__u8) (rpm / 100);
+	int max_allowed = get_max_fan_rpm(product_id);
+	if (rpm > max_allowed) {
+		return (__u8) (max_allowed / 100);
+	} else if (rpm < ABSOLUTE_MIN_FAN_RPM) {
+		return (__u8) (ABSOLUTE_MIN_FAN_RPM / 100);
+	}
+	return (__u8) (rpm / 100);
 }
 
 int creatorModeAllowed(__u32 product_id)
 {
-     switch (product_id)
-    {
-        case BLADE_2019_ADV:
-        case BLADE_2019_MERC:
-            return 1;
-        default:
-            return 0;
-    }
+	switch (product_id) {
+	case BLADE_2019_ADV:
+	case BLADE_2019_MERC:
+		return 1;
+	default:
+		return 0;
+	}
 }
