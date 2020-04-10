3c23dd1c55c5784b87f8cada1369c5f5c9999bd7
Ashcon Mohseninia <ashconm@outlook.com>
2020-04-10 10:47:23 +0100

    Update to version 1.0.3
    * Adds support for blade 2019 base model

--- driver/src/razer_common.c
+++ driver/src/razer_common.c
@@ -13,7 +13,7 @@
 MODULE_AUTHOR("Ashcon Mohseninia");
 MODULE_DESCRIPTION("Razer system control driver for laptops");
 MODULE_LICENSE("GPL");
-MODULE_VERSION("1.0.2");
+MODULE_VERSION("1.0.3");
 
 /**
  * Function to send RGB data to keyboard to display
