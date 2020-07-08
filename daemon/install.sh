332df6403b6770630dd38e849ae505da70b3578f
Ashcon Mohseninia <ashconm@outlook.com>
2020-07-08 11:48:03 +0100

    Removed dead code

--- daemon/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-/usr/bin/cp razercontrol-daemon /usr/bin/
-chmod +x /usr/bin/razercontrol-daemon
-/usr/bin/cp razerlaptop-control.service /etc/systemd/system/
-systemctl enable razerlaptop-control
-systemctl start razerlaptop-control
