9b17b9215c1b9dd12bf84fe0b258c85373d63145
Ashcon Mohseninia <ashconm@outlook.com>
2020-07-17 22:42:05 +0100

    Move razer-cli to /usr/bin/

--- razer_control_gui/install.sh
+++ razer_control_gui/install.sh
@@ -5,6 +5,7 @@ cargo build --release
 sudo /bin/bash << EOF
 mkdir -p /usr/share/razercontrol
 systemctl stop razerdaemon.service
+cp target/release/razer-cli /usr/bin/
 cp target/release/daemon /usr/share/razercontrol/
 cp razerdaemon.service /etc/systemd/system/
 systemctl enable razerdaemon.service
